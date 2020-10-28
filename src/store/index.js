d95154b599047ab98c8cc52231f6914497cfc512
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-10-27 21:15:08 -0300

    get data from firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    juguetes: []
   },
   mutations: {
+    GET_TOYS(state , juguetes){
+      state.juguetes = juguetes
+    }
   },
   actions: {
+    getToys({commit}){
+      firebase
+        .firestore()
+        .collection('juguetes')
+        .onSnapshot(snapshot => {
+          let juguetes = []
+          snapshot.forEach(juguete => {
+            juguetes.push({
+              id: juguete.id,
+              data: juguete.data()
+            })
+          })
+          commit('GET_TOYS', juguetes)
+        })
+    }
   },
   modules: {
   }
